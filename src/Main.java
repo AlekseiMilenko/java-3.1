6f1778220dec96b6c3bf8f452e29fc28f2dff147
Aleksei Milenko <amylenko@mail.ru>
2021-10-03 14:25:49 +0300

    Code reformated

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int miles = service.calculate(109_005);
+        if (miles > 1000)
+            miles = 1000;
         System.out.println(miles);
     }
 }
